bfeb09ab53caf5d485aa766adf919d05b50ab920
Kadson Lima <kadson-kaio@hotmail.com>
2023-05-22 22:27:00 -0300

    feat: home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,16 @@
 
+import { Box, Flex } from "@chakra-ui/react"
 import Header from "../../components/Header/Header"
 
 export function Home(){
 
 
-    return <Header></Header>
+    return <>
+    <Header/>
+    <Flex bg="#dc0a2d" marginTop="108px" height="calc(100vh - 108px)" padding={"10px"}>
+        <Box bg="#ffffff" width={"100%"} borderRadius={10} boxShadow={`inset 0 0 4px 4px #22222268`}>
+
+        </Box>
+    </Flex>
+    </>
 }
