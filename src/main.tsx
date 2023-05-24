f525ea3588944276fd09d9ad11fb67c9adffd4d9
Kadson Lima <kadson-kaio@hotmail.com>
2023-05-24 17:23:34 -0300

    fix: remove react toastify

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -9,7 +8,6 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <>
-    <ToastContainer position="top-right" rtl={false} />
     <Provider store={store}>
     <App />
     </Provider>
